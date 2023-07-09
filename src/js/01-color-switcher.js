8b47f254c8f77b4154cafaef6f341e13127692c7
MenkivOleg <laures04@gmail.com>
2023-07-09 22:09:06 +0300

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function startColorSwitching() {
+    startButton.disabled = true; 
+    stopButton.disabled = false;
+  intervalId = setInterval(changeColor, 1000); // Запускаємо зміну кольору кожну секунду
+}
+
+
+function stopColorSwitching() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  clearInterval(intervalId); // Зупиняємо зміну кольору
+}
+
+startButton.addEventListener('click', startColorSwitching);
+
+stopButton.addEventListener('click', stopColorSwitching);
